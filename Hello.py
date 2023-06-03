e54d1e85ab273bc0997bafecbe87a8f59546b532
mihirvaste <mailformihir521@gmail.com>
2023-06-03 21:01:45 +0530

    update python file in master

--- Hello.py
+++ Hello.py
@@ -1,3 +1,15 @@
+from flask import Flask
+import os
 
-for i in range(0,7):
-    print("Hello World!")
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello world!!!"
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
